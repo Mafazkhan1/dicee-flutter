6bb78e33186b006a58726031d472e7b0bda846a7
Mafazkhan1 <97543100+Mafazkhan1@users.noreply.github.com>
2022-02-02 16:41:34 +0530

    Dice app
    just completed demo of dice app
    
    Co-Authored-By: Mafazkhan1 <97543100+MafazKhan1@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftImageNumber = 5;
+  int rightImageNumber = 5;
+
+  void getNumber() {
+    leftImageNumber = Random().nextInt(6) + 1;
+    rightImageNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  getNumber();
+                });
+              },
+              child: Image.asset('images/dice$leftImageNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  getNumber();
+                });
+              },
+              child: Image.asset('images/dice$rightImageNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
